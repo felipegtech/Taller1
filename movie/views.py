9f40dc03620c3451326361753b512cfaa4492188
felipegtech <felipegomezd26@outlook.com>
2025-07-31 11:47:55 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name': 'Felipe Gomez'})
+
+def about(request):
+    return HttpResponse("<h1> Welcome to About page</h1>")
+
